434c3a99d7a6912aab5dcf9144a90a1576ef1263
OneByte <onebytecodes@gmail.com>
2022-07-10 20:52:28 +0530

    Added all endpoints and it works perfectly

--- index.js
+++ index.js
@@ -1,12 +1,39 @@
-const express = require('express')
+const express = require('express');
+const sunTzu = require('sun-tzu-quotes');
 const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("./swagger.json");
-const app = express()
+const app = express();
 
 app.get('/', (req, res) => {
-})
+    res.redirect('/api-docs');
+});
 
 app.use('/api-docs', swaggerUi.serve);
 app.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
-app.listen(3000, () => console.log(`Sun Tzu Quotes API listening on port 3000`))
+app.get("/quote", (req, res) => {
+    res.status(200).json({
+        "quote": sunTzu()
+    });
+});
+
+app.get("/quote/:id", (req, res) => {
+    const id = req.params.id;
+
+    if(id > 0 && id < sunTzu.quotes.length) {
+        res.status(200).json({
+            "quote": sunTzu.quotes[id]
+        });
+    } else {
+        res.status(404).json({
+            "error": "Quote not found"
+        });
+    }
+});
+
+app.get("/quotes", (req, res) => {
+    res.status(200).json(sunTzu.quotes);
+});
+
+
+app.listen(3000, () => console.log(`Sun Tzu Quotes API listening on port 3000`));
