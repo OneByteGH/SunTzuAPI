25daa24aa8f82e4219a5a03a3e0503c1ff4c6191
OneByte <onebytecodes@gmail.com>
2022-07-10 19:48:45 +0530

    Added Swagger for documentation

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express')
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
 })
+
+app.use('/api-docs', swaggerUi.serve);
+app.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
 app.listen(3000, () => {
     console.log(`Sun Tzu Quotes API listening on port 3000`)
